5add170c68f35c41b90610d4692dca9af83997c3
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2024-11-11 18:09:11 +0100

    eslint@9

--- tests/test.mjs
+++ tests/test.mjs
@@ -1,18 +1,18 @@
-import assert from 'assert';
-import { eot2ttf } from '../dist/bindings.js';
-import fs from 'fs';
+import assert from "assert";
+import { eot2ttf } from "../dist/bindings.js";
+import fs from "fs";
 
 function testDecoderOS2() {
-    var src = new Uint8Array(fs.readFileSync('./tests/fixtures/test_v0.eot'))
-    var dst = new Uint8Array(fs.readFileSync('./tests/fixtures/test_v0.ttf'))
+    var src = new Uint8Array(fs.readFileSync("./tests/fixtures/test_v0.eot"));
+    var dst = new Uint8Array(fs.readFileSync("./tests/fixtures/test_v0.ttf"));
 
-    assert.deepEqual(eot2ttf(src), dst)
+    assert.deepEqual(eot2ttf(src), dst);
 }
 function testDecoder() {
-    var src = new Uint8Array(fs.readFileSync('./tests/fixtures/test.eot'))
-    var dst = new Uint8Array(fs.readFileSync('./tests/fixtures/test.ttf'))
+    var src = new Uint8Array(fs.readFileSync("./tests/fixtures/test.eot"));
+    var dst = new Uint8Array(fs.readFileSync("./tests/fixtures/test.ttf"));
 
-    assert.deepEqual(eot2ttf(src), dst)
+    assert.deepEqual(eot2ttf(src), dst);
 }
 
 assert.doesNotThrow(testDecoderOS2, undefined, "testDecoderOS2 threw an exception");
